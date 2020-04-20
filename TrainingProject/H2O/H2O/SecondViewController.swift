4b6acac62433b28dae844f54ccdce2a96313017d
MikovozSergey <36566639+MikovozSergey@users.noreply.github.com>
2020-04-20 17:15:45 +0300

    add test version of my app

---
+++ TrainingProject/H2O/H2O/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  H2O
+//
+//  Created by Дарья Станкевич on 4/20/20.
+//  Copyright © 2020 Sergey Mikovoz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
